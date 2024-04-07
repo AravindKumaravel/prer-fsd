d60152815fafc174497577637659e6b353d60a76
AravindKumaravel <aravindkumaravel200023@gmail.com>
2024-04-07 12:55:16 +0530

    First React Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <greet/>
+    </>
+  )
+}
+function greet(){
+  return(
+    <div>
+      <h1>Welcome to react</h1>
+    </div>
+
+  );
+}
+export default App
